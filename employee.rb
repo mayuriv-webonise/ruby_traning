488c3061b3a2e11acf2294e2a20d690c5875cb7d
mayuriv-webonise <mayuri.vibhute@webonise.com>
2023-08-16 16:52:04 +0530

    alignment added

--- employee.rb
+++ employee.rb
@@ -1,19 +1,19 @@
-require("./assignment3.rb")
-  class Employee
-    attr_reader :name, :age, :department, :division, :designation
+  require("./assignment3.rb")
+    class Employee
+      attr_reader :name, :age, :department, :division, :designation
 
-    def initialize(name, age, department, designation)
-      @name = name
-      @age = age
-      @department = department
-      @division = division
-      @designation = designation
-    end
+      def initialize(name, age, department, designation)
+        @name = name
+        @age = age
+        @department = department
+        @division = division
+        @designation = designation
+      end
 
-    def to_csv_row
-      [name, age, department, division, designation]
+      def to_csv_row
+        [name, age, department, division, designation]
+      end
     end
-  end
 
 
 
