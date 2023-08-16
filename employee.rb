39a69b155391f862e8b0d55880bb9413bdaecb86
mayuriv-webonise <mayuri.vibhute@webonise.com>
2023-08-16 16:58:46 +0530

    removed require

--- employee.rb
+++ employee.rb
@@ -1,19 +1,18 @@
-  require("./assignment3.rb")
-    class Employee
-      attr_reader :name, :age, :department, :division, :designation
+  class Employee
+        attr_reader :name, :age, :department, :division, :designation
 
-      def initialize(name, age, department, designation)
-        @name = name
-        @age = age
-        @department = department
-        @division = division
-        @designation = designation
-      end
+        def initialize(name, age, department, designation)
+          @name = name
+          @age = age
+          @department = department
+          @division = division
+          @designation = designation
+        end
 
-      def to_csv_row
-        [name, age, department, division, designation]
-      end
-    end
+        def to_csv_row
+          [name, age, department, division, designation]
+        end
+  end
 
 
 
--- factory_method.rb
+++ factory_method.rb
@@ -1,21 +1,19 @@
-  require("./assignment3.rb")
-
   # Subclasses for different divisions
-  class Development < Employee; end
-  class QA < Employee; end
-  class Devops < Employee; end
+    class Development < Employee; end
+    class QA < Employee; end
+    class Devops < Employee; end
 
-  class EmployeeFactory
-    def create_employee(name, age, department, division, designation)
-      case division
-      when 'Development'
-        Development.new(name, age, department, designation)
-      when 'QA'
-        QA.new(name, age, department, designation)
-      when 'Devops'
-        Devops.new(name, age, department, designation)
-      else
-        Employee.new(name, age, department, designation)
+    class EmployeeFactory
+      def create_employee(name, age, department, division, designation)
+        case division
+        when 'Development'
+          Development.new(name, age, department, designation)
+        when 'QA'
+          QA.new(name, age, department, designation)
+        when 'Devops'
+          Devops.new(name, age, department, designation)
+        else
+          Employee.new(name, age, department, designation)
+        end
       end
     end
-  end
