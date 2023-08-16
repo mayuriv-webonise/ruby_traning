8ccb29e6624dfda28457f58310c923bd6f3394f2
mayuriv-webonise <mayuri.vibhute@webonise.com>
2023-08-16 16:53:51 +0530

    alignment for other 2 file

--- department.rb
+++ department.rb
@@ -1,18 +1,18 @@
-  class Department
-    attr_reader :name, :department_head, :employees
+    class Department
+      attr_reader :name, :department_head, :employees
 
-    def initialize(name, department_head)
-      @name = name
-      @department_head = department_head
-      @employees = []
-    end
+      def initialize(name, department_head)
+        @name = name
+        @department_head = department_head
+        @employees = []
+      end
 
-    def add_employee(employee)
-      @employees << employee
-    end
+      def add_employee(employee)
+        @employees << employee
+      end
 
-    def get_department_employees
-      @employees
+      def get_department_employees
+        @employees
+      end
     end
-  end
 
